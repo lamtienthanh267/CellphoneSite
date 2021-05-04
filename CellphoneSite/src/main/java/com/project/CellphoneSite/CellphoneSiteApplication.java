2585512fad4f963878ae1e6637dbf2d52db89006
Lam Tien Thanh <79984087+lamtienthanh267@users.noreply.github.com>
2021-05-04 14:39:10 +0700

    + Create project & edit pom.xml

---
+++ CellphoneSite/src/main/java/com/project/CellphoneSite/CellphoneSiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.CellphoneSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellphoneSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellphoneSiteApplication.class, args);
+	}
+
+}
---
+++ CellphoneSite/src/test/java/com/project/CellphoneSite/CellphoneSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CellphoneSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellphoneSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
